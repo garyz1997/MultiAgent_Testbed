16111acad7a03b64905977056212b48f785877e2
Gary <garyz1997@yahoo.com>
2018-04-11 15:25:26 -0300

    Logic Update

--- src/Agents/PartAgent.java
+++ src/Agents/PartAgent.java
@@ -15,6 +15,7 @@ public class PartAgent extends Agent
 	String processNumber=null;
 	String partNumber=null;
 	int location = 1;//1 RFIDbegin, 2 CNCbegin, 3 CNCend, 4 RFIDend, 5 exit
+	boolean lock = false;//waiting for message
 
 	protected void setup()
 	{
@@ -26,31 +27,11 @@ public class PartAgent extends Agent
 			partNumber = (String) args[1];
 		}
 
-
-		if (processNumber != "3" || partNumber == "1")//part doesn't need anything here
-		{//tell RFID to send sled
-			System.out.println("Sending message to RFID Agent to let pallet pass");
-			ACLMessage releasePart = new ACLMessage( ACLMessage.REQUEST );
-			releasePart.addReceiver(new AID("rfid3Agent", AID.ISLOCALNAME));
-			releasePart.setContent("Release Pallet");
-			send(releasePart);
-		}
-		else//part needs a process here. For now, send part# 2 to CNC 3, and part# 4 to CNC 4
-		{
-			if (partNumber == "2")
-			{//tell ROBOT to move part to CNC3
-
-				location = 2;
-			}
-			else if (partNumber == "3")
-			{//tell ROBOT to move part to CNC4
-
-				location = 2;
-			}
-		}
-
 		// Add the logic behavior
 		addBehaviour(new partLogic());
+
+		// Add the behaviour serving busy messages from the robot
+		addBehaviour(new BusyHandler());
 	}
 	
 	private class partLogic extends TickerBehaviour
@@ -63,15 +44,54 @@ public class PartAgent extends Agent
 		{
 			switch (location) 
 			{
+			case 1://RFIDBegin
+				if (processNumber != "3" || partNumber == "1")//part doesn't need anything here
+				{//tell RFID to send sled
+					location = 4;
+				}
+				else//part needs a process here. For now, send part# 2 to CNC 3, and part# 3 to CNC 4
+				{
+					if (partNumber == "2")
+					{//tell ROBOT to move part to CNC3, check robot is free, cnc is free 
+						System.out.println("Sending message to ROBOT to check if it and CNC3 are free");
+						ACLMessage checkRobotCNCFree = new ACLMessage( ACLMessage.REQUEST );
+						checkRobotCNCFree.addReceiver(new AID("RobotAgent", AID.ISLOCALNAME));
+						checkRobotCNCFree.setContent("Check CNC3 Free");
+						send(checkRobotCNCFree);
+					}
+					else if (partNumber == "3")
+					{//tell ROBOT to move part to CNC4
+						System.out.println("Sending message to ROBOT to check if it and CNC4 are free");
+						ACLMessage checkRobotCNCFree = new ACLMessage( ACLMessage.REQUEST );
+						checkRobotCNCFree.addReceiver(new AID("RobotAgent", AID.ISLOCALNAME));
+						checkRobotCNCFree.setContent("Check CNC4 Free");
+						send(checkRobotCNCFree);
+					}
+				}
+				break;
 			case 2://CNCbegin
 				break;
 			case 3://CNCend
+				System.out.println("Sending message to ROBOT to check if it and PALLET are free");
+				ACLMessage checkRobotCNCFree = new ACLMessage( ACLMessage.REQUEST );
+				checkRobotCNCFree.addReceiver(new AID("RobotAgent", AID.ISLOCALNAME));
+				checkRobotCNCFree.setContent("Check PALLET Free");
+				send(checkRobotCNCFree);
 				break;
 			case 4://RFIDend
+				System.out.println("Sending message to RFID Agent to let pallet pass");
+				ACLMessage releasePart = new ACLMessage( ACLMessage.REQUEST );
+				releasePart.addReceiver(new AID("rfid3Agent", AID.ISLOCALNAME));
+				releasePart.setContent("Release Pallet");
+				send(releasePart);
+				location = 5;
 				break;
 			case 5://Exit
+				myAgent.doDelete();
 				break;
-
+			default:
+				System.out.println("Location Error");
+				break;
 			}
 		}
 	}
@@ -81,4 +101,34 @@ public class PartAgent extends Agent
 		System.out.println(getAID().getName()+" terminating.");
 	}
 
+	/**
+	   Inner class BusyHandler().
+	   This is the behaviour used by the PA to handle when a busy message is received
+	 */
+	private class BusyHandler extends CyclicBehaviour 
+	{
+		public void action() 
+		{
+			MessageTemplate mt = MessageTemplate.MatchContent("busy");
+			ACLMessage msg = myAgent.receive(mt);
+			if (msg != null) 
+			{
+				if (location == 1)
+				{
+					System.out.println("The ROBOT or CNC are busy, exiting Cell 2");
+					location = 4;//RFIDend
+				}
+				else if (location == 3)
+				{
+					System.out.println("The ROBOT or PALLET are currently busy");
+				}
+			}
+			else 
+			{
+				block();
+			}
+		}
+	}  // End of inner class BusyHandler
+
 }
+//1 RFIDbegin, 2 CNCbegin, 3 CNCend, 4 RFIDend, 5 exit
